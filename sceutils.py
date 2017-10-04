956630545d323f548d1fc7a364eeed533bde56b6
molecule <molecule>
2017-10-04 09:00:04 -0700

    2 spaces => 4 spaces

--- sceutils.py
+++ sceutils.py
@@ -3,30 +3,30 @@ from Crypto.Util import Counter
 from scetypes import *
 
 def print_metadata_keyvault(keys):
-  print 'Metadata Vault:'
-  for k in keys:
-    print ' {0}:              0x{1:X}'.format(''.join('{:02X}'.format(x) for x in k))
+    print 'Metadata Vault:'
+    for k in keys:
+        print ' {0}:              0x{1:X}'.format(''.join('{:02X}'.format(x) for x in k))
 
 def get_keys(inf, sce_hdr, sysver, self_type=SelfType.NONE, silent=False):
-  from keys import SCE_KEYS
-  inf.seek(sce_hdr.metadata_offset + 48)
-  dat = inf.read(sce_hdr.header_length - sce_hdr.metadata_offset - 48)
-  (key, iv) = SCE_KEYS.get(sce_hdr.sce_type, sysver, self_type)
-  hdr_dec = AES.new(key, AES.MODE_CBC, iv)
-  dec = hdr_dec.decrypt(dat[0:MetadataInfo.Size])
-  metadata_info = MetadataInfo(dec)
-  if not silent:
-    print metadata_info
-  contents_dec = AES.new(metadata_info.key, AES.MODE_CBC, metadata_info.iv)
-  dec = contents_dec.decrypt(dat[MetadataInfo.Size:])
-  metadata_hdr = MetadataHeader(dec[0:MetadataHeader.Size])
-  if not silent:
-    print metadata_hdr
-  keymap = {}
-  vault = [dec[MetadataHeader.Size + metadata_hdr.section_count*MetadataSection.Size + 16*i:MetadataHeader.Size + metadata_hdr.section_count*MetadataSection.Size + 16*i + 16] for i in range(metadata_hdr.key_count)]
-  for i in range(metadata_hdr.section_count):
-    metsec = MetadataSection(dec[MetadataHeader.Size + i*MetadataSection.Size:MetadataHeader.Size + i*MetadataSection.Size + MetadataSection.Size])
+    from keys import SCE_KEYS
+    inf.seek(sce_hdr.metadata_offset + 48)
+    dat = inf.read(sce_hdr.header_length - sce_hdr.metadata_offset - 48)
+    (key, iv) = SCE_KEYS.get(sce_hdr.sce_type, sysver, self_type)
+    hdr_dec = AES.new(key, AES.MODE_CBC, iv)
+    dec = hdr_dec.decrypt(dat[0:MetadataInfo.Size])
+    metadata_info = MetadataInfo(dec)
     if not silent:
-      print metsec
-    keymap[metsec.seg_idx] = (vault[metsec.key_idx], vault[metsec.iv_idx])
-  return keymap
+        print metadata_info
+    contents_dec = AES.new(metadata_info.key, AES.MODE_CBC, metadata_info.iv)
+    dec = contents_dec.decrypt(dat[MetadataInfo.Size:])
+    metadata_hdr = MetadataHeader(dec[0:MetadataHeader.Size])
+    if not silent:
+        print metadata_hdr
+    keymap = {}
+    vault = [dec[MetadataHeader.Size + metadata_hdr.section_count*MetadataSection.Size + 16*i:MetadataHeader.Size + metadata_hdr.section_count*MetadataSection.Size + 16*i + 16] for i in range(metadata_hdr.key_count)]
+    for i in range(metadata_hdr.section_count):
+        metsec = MetadataSection(dec[MetadataHeader.Size + i*MetadataSection.Size:MetadataHeader.Size + i*MetadataSection.Size + MetadataSection.Size])
+        if not silent:
+            print metsec
+        keymap[metsec.seg_idx] = (vault[metsec.key_idx], vault[metsec.iv_idx])
+    return keymap
--- self2elf.py
+++ self2elf.py
@@ -8,84 +8,84 @@ from Crypto.Cipher import AES
 from Crypto.Util import Counter
 
 def self2elf(inf, outf=DevNull(), silent=False):
-  dat = inf.read(SceHeader.Size)
-  sce = SceHeader(dat)
-  if not silent:
-    print sce
-  dat = inf.read(SelfHeader.Size)
-  self_hdr = SelfHeader(dat)
-  inf.seek(self_hdr.appinfo_offset)
-  dat = inf.read(AppInfoHeader.Size)
-  appinfo_hdr = AppInfoHeader(dat)
-  if not silent:
-    print appinfo_hdr
-  # copy elf header
-  inf.seek(self_hdr.elf_offset)
-  dat = inf.read(ElfHeader.Size)
-  outf.write(dat)
-  elf_hdr = ElfHeader(dat)
-  if not silent:
-    print elf_hdr
-  # get segments
-  elf_phdrs = []
-  segment_infos = []
-  encrypted = False
-  at = ElfHeader.Size
-  for i in range(elf_hdr.e_phnum):
-    # phdr
-    inf.seek(self_hdr.phdr_offset + i*ElfPhdr.Size)
-    dat = inf.read(ElfPhdr.Size)
-    phdr = ElfPhdr(dat)
+    dat = inf.read(SceHeader.Size)
+    sce = SceHeader(dat)
     if not silent:
-      print phdr
-    elf_phdrs.append(phdr)
-    # write phdr
+        print sce
+    dat = inf.read(SelfHeader.Size)
+    self_hdr = SelfHeader(dat)
+    inf.seek(self_hdr.appinfo_offset)
+    dat = inf.read(AppInfoHeader.Size)
+    appinfo_hdr = AppInfoHeader(dat)
+    if not silent:
+        print appinfo_hdr
+    # copy elf header
+    inf.seek(self_hdr.elf_offset)
+    dat = inf.read(ElfHeader.Size)
     outf.write(dat)
-    at += ElfPhdr.Size
-    # seg info
-    inf.seek(self_hdr.segment_info_offset + i*SegmentInfo.Size)
-    dat = inf.read(SegmentInfo.Size)
-    segment_info = SegmentInfo(dat)
+    elf_hdr = ElfHeader(dat)
     if not silent:
-      print segment_info
-    segment_infos.append(segment_info)
-    if segment_info.plaintext == SecureBool.NO:
-      encrypted = True
-  # get keys
-  if encrypted:
-    keys = get_keys(inf, sce, appinfo_hdr.sys_version, appinfo_hdr.self_type, silent)
-  else:
-    keys = {}
-  # generate ELF
-  for i in range(elf_hdr.e_phnum):
-    if elf_phdrs[i].p_filesz == 0:
-      continue
-    if not silent:
-      print 'Dumping segment {0}...'.format(i)
-    # padding
-    pad_len = elf_phdrs[i].p_offset - at
-    if pad_len < 0:
-      raise RuntimeError("ELF p_offset invalid!")
-    outf.write(bytearray([0] * pad_len))
-    at += pad_len
-    # data
-    inf.seek(segment_infos[i].offset)
-    dat = inf.read(segment_infos[i].size)
-    # encryption
-    if segment_infos[i].plaintext == SecureBool.NO:
-      (key, iv) = keys[i]
-      ctr = Counter.new(128, initial_value=long(iv.encode("hex"), 16))
-      section_aes = AES.new(key, AES.MODE_CTR, counter=ctr)
-      dat = section_aes.decrypt(dat)
-    # compression
-    if segment_infos[i].compressed == SecureBool.YES:
-      z = zlib.decompressobj()
-      dat = z.decompress(dat)
-    # write-back
-    outf.write(dat)
-    at += len(dat)
+        print elf_hdr
+    # get segments
+    elf_phdrs = []
+    segment_infos = []
+    encrypted = False
+    at = ElfHeader.Size
+    for i in range(elf_hdr.e_phnum):
+        # phdr
+        inf.seek(self_hdr.phdr_offset + i*ElfPhdr.Size)
+        dat = inf.read(ElfPhdr.Size)
+        phdr = ElfPhdr(dat)
+        if not silent:
+            print phdr
+        elf_phdrs.append(phdr)
+        # write phdr
+        outf.write(dat)
+        at += ElfPhdr.Size
+        # seg info
+        inf.seek(self_hdr.segment_info_offset + i*SegmentInfo.Size)
+        dat = inf.read(SegmentInfo.Size)
+        segment_info = SegmentInfo(dat)
+        if not silent:
+            print segment_info
+        segment_infos.append(segment_info)
+        if segment_info.plaintext == SecureBool.NO:
+            encrypted = True
+    # get keys
+    if encrypted:
+        keys = get_keys(inf, sce, appinfo_hdr.sys_version, appinfo_hdr.self_type, silent)
+    else:
+        keys = {}
+    # generate ELF
+    for i in range(elf_hdr.e_phnum):
+        if elf_phdrs[i].p_filesz == 0:
+            continue
+        if not silent:
+            print 'Dumping segment {0}...'.format(i)
+        # padding
+        pad_len = elf_phdrs[i].p_offset - at
+        if pad_len < 0:
+            raise RuntimeError("ELF p_offset invalid!")
+        outf.write(bytearray([0] * pad_len))
+        at += pad_len
+        # data
+        inf.seek(segment_infos[i].offset)
+        dat = inf.read(segment_infos[i].size)
+        # encryption
+        if segment_infos[i].plaintext == SecureBool.NO:
+            (key, iv) = keys[i]
+            ctr = Counter.new(128, initial_value=long(iv.encode("hex"), 16))
+            section_aes = AES.new(key, AES.MODE_CTR, counter=ctr)
+            dat = section_aes.decrypt(dat)
+        # compression
+        if segment_infos[i].compressed == SecureBool.YES:
+            z = zlib.decompressobj()
+            dat = z.decompress(dat)
+        # write-back
+        outf.write(dat)
+        at += len(dat)
 
 if __name__ == "__main__":
-  with open(sys.argv[1], "rb") as inf:
-    with open(sys.argv[2], "wb") as outf:
-      self2elf(inf, outf)
+    with open(sys.argv[1], "rb") as inf:
+        with open(sys.argv[2], "wb") as outf:
+            self2elf(inf, outf)
