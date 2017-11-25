8fe327163722437fa188063ae459a7470f18b3d3
Ilya Zhuravlev <whatever@xyz.is>
2017-11-25 23:14:36 +0300

    vita_loader: refactor imp/exp to reduce code duplication

--- vita_loader.py
+++ vita_loader.py
@@ -6,7 +6,6 @@ from collections import defaultdict
 
 import idaapi
 import idc
-import ida_bytes
 
 
 NORETURN_NIDS = [0xB997493D, 0x391B5B74, 0x00CCE39C, 0x37691BF8, 0x2F2C6046, 0x39AD080B, 0x83A4F46F, 0xB1CD7AC2, 0xEC287338]
@@ -31,24 +30,7 @@ def c_str(data):
     return data[:data.find("\x00")]
 
 
-g_names = dict()
-
-def load_nids():
-    print "0) Building NID cache..."
-    try:
-        with open("db.yml", "r") as fin:
-            data = fin.read().split("\n")
-    except IOError:
-        raise Exception("Please place db.yml into the directory with your elfs!")
-    for line in data:
-        if "0x" in line and "nid: " not in line:
-            name, nid = line.strip().split(":")
-            name = name.strip()
-            nid = int(nid.strip(), 16)
-            g_names[nid] = name
-
-
-class ELFHeader():
+class ELFHeader:
 
     def __init__(self, data):
         self.e_entry = u32(data, 0x18)
@@ -57,7 +39,7 @@ class ELFHeader():
         self.e_phnum = u16(data, 0x2C)
 
 
-class ELFphdr():
+class ELFphdr:
 
     def __init__(self, data):
         self.p_type = u32(data, 0)
@@ -69,7 +51,7 @@ class ELFphdr():
         self.x = bool(self.p_flags & 1)
 
 
-class Modinfo():
+class Modinfo:
 
     def __init__(self, data):
         self.sz = u32(data, 0)
@@ -79,7 +61,9 @@ class Modinfo():
         self.import_end = u32(data, 0x30)
 
 
-class Modexport():
+class Modexport:
+
+    required_reading = 0x20
 
     def __init__(self, data):
         self.sz = u8(data, 0)
@@ -92,7 +76,9 @@ class Modexport():
         self.entry_table = u32(data, 28)
 
 
-class Modimport():
+class Modimport:
+
+    required_reading = 0x24
 
     def __init__(self, data):
         self.sz = u16(data, 0)
@@ -116,109 +102,128 @@ def make_func(func, name):
 
 def func_flags(func, nid):
     if nid in NORETURN_NIDS:
-        idc.SetFunctionFlags(func, idc.GetFunctionFlags(func) | FUNC_NORET)
+        idc.SetFunctionFlags(func, idc.GetFunctionFlags(func) | idaapi.FUNC_NORET)
 
 
-g_comments = defaultdict(list)
-def add_nid_cmt(func, cmt):
-    func = func & ~1
-    g_comments[func].append(cmt)
-    idc.set_func_cmt(func, " aka ".join(g_comments[func]), 0)
+class VitaElf:
+
+    def __init__(self, fin):
+        self.fin = fin
+        self.nid_to_name = dict()
+        self.seg0_off = None
+        self.seg0_va = None
+        self.comments = defaultdict(list)
+
+    def load_nids(self):
+        try:
+            with open("db.yml", "r") as fin:
+                data = fin.read().split("\n")
+        except IOError:
+            raise Exception("Please place db.yml into the directory with your elfs!")
+        for line in data:
+            if "0x" in line and "nid: " not in line:
+                name, nid = line.strip().split(":")
+                name = name.strip()
+                nid = int(nid.strip(), 16)
+                self.nid_to_name[nid] = name
+
+    def add_nid_cmt(self, func, cmt):
+        func = func & ~1
+        self.comments[func].append(cmt)
+        idc.set_func_cmt(func, " aka ".join(self.comments[func]), 0)
+
+    def parse_impexp(self, top, end, cls, callback):
+        arr = []
+        cur = top
+        while cur < end:
+            self.fin.seek(self.seg0_off + cur)
+            impexp = cls(self.fin.read(cls.required_reading))
+            if impexp.libname_ptr:
+                self.fin.seek(self.seg0_off + impexp.libname_ptr - self.seg0_va)
+                data = self.fin.read(256)
+                impexp.libname = c_str(data)
+
+            arr.append(impexp)
+            cur += impexp.sz
+
+        for impexp in arr:
+            for x in xrange(impexp.num_funcs):
+                self.fin.seek(self.seg0_off + impexp.nid_table - self.seg0_va + x * 4)
+                nid = u32(self.fin.read(4))
+                self.fin.seek(self.seg0_off + impexp.entry_table - self.seg0_va + x * 4)
+                func = u32(self.fin.read(4))
+
+                callback(impexp, func, nid)
+
+    def func_get_name(self, prefix, libname, nid):
+        if nid in self.nid_to_name:
+            suffix = self.nid_to_name[nid]
+        else:
+            suffix = "0x{:08X}".format(nid)
+        return "{}.{}.{}".format(prefix, libname, suffix)
+
+    def cb_exp(self, exp, func, nid):
+        name = self.func_get_name("exp", exp.libname, nid)
+
+        make_func(func, name)
+        func_flags(func, nid)
+
+        self.add_nid_cmt(func, "[Export libnid: 0x{:08X} ({}), NID: 0x{:08X}]".format(exp.libnid, exp.libname, nid))
+
+    def cb_imp(self, imp, func, nid):
+        name = self.func_get_name("imp", imp.libname, nid)
+
+        make_func(func, name)
+        idc.SetFunctionFlags(func, idaapi.FUNC_THUNK | idaapi.FUNC_LIB)
+        func_flags(func, nid)
+
+        self.add_nid_cmt(func, "[Import libnid: 0x{:08X} ({}), NID: 0x{:08X}]".format(imp.libnid, imp.libname, nid))
+
+    def go(self):
+        print "0) Building NID cache..."
+        self.load_nids()
+
+        # Vita is ARM
+        idaapi.set_processor_type("arm", idaapi.SETPROC_ALL|idaapi.SETPROC_FATAL)
+
+        print "1) Loading ELF segments"
+        self.fin.seek(0)
+        header = ELFHeader(self.fin.read(0x34))
+
+        self.fin.seek(header.e_phoff)
+        phdrs = [ELFphdr(self.fin.read(header.e_phentsize)) for _ in xrange(header.e_phnum)]
+
+        for phdr in phdrs:
+            if phdr.p_type == p_type.PT_LOAD:
+                idaapi.add_segm(0, phdr.p_vaddr, phdr.p_vaddr + phdr.p_memsz,
+                                ".text" if phdr.x else ".data",
+                                "CODE" if phdr.x else "DATA")
+                self.fin.file2base(phdr.p_offset, phdr.p_vaddr, phdr.p_vaddr + phdr.p_filesz, 1)
+
+        self.seg0_off = phdrs[0].p_offset
+        self.seg0_va = phdrs[0].p_vaddr
+
+        self.fin.seek(self.seg0_off + header.e_entry)
+        modinfo = Modinfo(self.fin.read(0x34))
+
+        print "2) Parsing export tables"
+        self.parse_impexp(modinfo.export_top, modinfo.export_end, Modexport, self.cb_exp)
+
+        print "3) Parsing import tables"
+        self.parse_impexp(modinfo.import_top, modinfo.import_end, Modimport, self.cb_imp)
+
+        print "4) Waiting for IDA to analyze the program"
+        idc.Wait()
 
 
-def load_file(fin, neflags, format):
-    load_nids()
-
-    # Vita is ARM
-    idaapi.set_processor_type("arm", SETPROC_ALL|SETPROC_FATAL)
-
-    print "1) Load ELF segments"
-
-    fin.seek(0)
-    header = ELFHeader(fin.read(0x34))
-
-    fin.seek(header.e_phoff)
-    phdrs = [ELFphdr(fin.read(header.e_phentsize)) for x in xrange(header.e_phnum)]
-
-    for phdr in phdrs:
-        if phdr.p_type == p_type.PT_LOAD:
-            idaapi.add_segm(0, phdr.p_vaddr, phdr.p_vaddr + phdr.p_memsz, ".text" if phdr.x else ".data", "CODE" if phdr.x else "DATA")
-            fin.file2base(phdr.p_offset, phdr.p_vaddr, phdr.p_vaddr + phdr.p_filesz, 1)
-
-    base = phdrs[0].p_offset
-    va_base = phdrs[0].p_vaddr
-
-    fin.seek(base + header.e_entry)
-    modinfo = Modinfo(fin.read(0x34))
-
-    print "2) Parse export tables"
-    exports = []
-    cur = modinfo.export_top
-    while cur < modinfo.export_end:
-        fin.seek(base + cur)
-        exp = Modexport(fin.read(0x20))
-        if exp.libname_ptr:
-            fin.seek(base + exp.libname_ptr - va_base)
-            data = fin.read(256)
-            exp.libname = c_str(data)
-
-        exports.append(exp)
-        cur += 0x20
-
-    for export in exports:
-        for x in xrange(export.num_funcs):
-            fin.seek(base + export.nid_table - va_base + x * 4)
-            nid = u32(fin.read(4))
-            fin.seek(base + export.entry_table - va_base + x * 4)
-            func = u32(fin.read(4))
-
-            if nid in g_names:
-                name = "exp.{}.{}".format(export.libname, g_names[nid])
-            else:
-                name = "exp.{}.0x{:08X}".format(export.libname, nid)
-
-            make_func(func, name)
-            func_flags(func, nid)
-
-            add_nid_cmt(func, "[Export libnid: 0x{:08X} ({}) NID: 0x{:08X}]".format(export.libnid, export.libname, nid))
-
-
-    print "3) Parse import tables"
-    imports = []
-    cur = modinfo.import_top
-    while cur < modinfo.import_end:
-        fin.seek(base + cur)
-        imp = Modimport(fin.read(0x80))
-        if imp.libname_ptr:
-            fin.seek(base + imp.libname_ptr - va_base)
-            data = fin.read(256)
-            imp.libname = c_str(data)
-
-        imports.append(imp)
-        cur += imp.sz
-
-    for imp in imports:
-        for x in xrange(imp.num_funcs):
-            fin.seek(base + imp.nid_table - va_base + x * 4)
-            nid = u32(fin.read(4))
-            fin.seek(base + imp.entry_table - va_base + x * 4)
-            func = u32(fin.read(4))
-
-            if nid in g_names:
-                name = "imp.{}.{}".format(imp.libname, g_names[nid])
-            else:
-                name = "imp.{}.0x{:08X}".format(imp.libname, nid)
-
-            make_func(func, name)
-            idc.SetFunctionFlags(func, FUNC_THUNK | FUNC_LIB)
-            func_flags(func, nid)
-
-            add_nid_cmt(func, "[Import libnid: 0x{:08X} ({}), NID: 0x{:08X}]".format(imp.libnid, imp.libname, nid))
+def load_file(fin, *args, **kwargs):
+    e = VitaElf(fin)
+    e.go()
 
     return 1
 
 
-def accept_file(fin, name):
+def accept_file(fin, *args, **kwargs):
     fin.seek(0)
     header = fin.read(0x34)
     if header.startswith("\x7fELF") and u16(header, 0x10) == 0xFE04:
